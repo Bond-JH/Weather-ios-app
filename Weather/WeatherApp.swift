6aea9bd0cc1bd90ecd900e1fa8500360e5fff855
Bond-JH <838629462@qq.com>
2025-09-25 16:35:18 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 杨家豪 on 2025/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
